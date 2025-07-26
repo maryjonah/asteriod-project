34c7ee335371af8727bc72871405bf7911f8566d
Mary Jonah <jonahmary17@gmail.com>
2025-07-26 06:03:00 +0000

    One: Created a black screen

--- main.py
+++ main.py
@@ -6,16 +6,13 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
-    color = (0,0,0)
-
-    while(True):
+    while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
 
-        screen.fill(color)
+        screen.fill("black")
         pygame.display.flip()
-
 
 
 if __name__ == "__main__":
